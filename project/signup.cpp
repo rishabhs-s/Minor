a32c8c133de34a5c236e3b4fa8bf3315f76ac0d2
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-09-10 15:29:51 +0530

    First commit

---
+++ project/signup.cpp
@@ -0,0 +1,19 @@
+#include "signup.h"
+#include "ui_signup.h"
+
+Signup::Signup(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Signup)
+{
+    ui->setupUi(this);
+}
+
+Signup::~Signup()
+{
+    delete ui;
+}
+
+void Signup::on_pushButton_clicked()
+{
+
+}
